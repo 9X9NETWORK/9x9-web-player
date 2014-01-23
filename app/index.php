521edc7f6cfb6982b060f71fe871e397672833b2
ubuntu <ubuntu@beagle.9x9.tv>
2014-01-23 10:39:43 +0000

    feeding large thumbnail

--- app/index.php
+++ app/index.php
@@ -1,5 +1,15 @@
 <?php
 
+    $ytChannelRegex = "/^http:\\/\\/www\\.youtube\\.com\\/user\\/(.*)$/";
+    $ytPlaylistRegex = "/^http:\\/\\/www\\.youtube\\.com\\/view_play_list\\?p=(.*)$/";
+    $ytApiKey = "AI39si7lCrdz_clsMg5Ssip-jeguuZIZZ7Zev-pz35ay43sH2ApxagMgtUOa6j3kWpcOU-2P-ERJ44oLwRdbidDJJlPR2nKCGw";
+    $ytGdata = "http://gdata.youtube.com";
+    $ytParam = "v=2&alt=json&key=$ytApiKey";
+
+    function compare_thumbnail($thumb1, $thumb2) {
+        return $thumb1['width'] - $thumb2['width'];
+    }
+
     function htmlsafe($str) {
 
         if ($str)
@@ -17,60 +27,90 @@
     $ch = isset($_GET['ch']) ? $_GET['ch'] : null;
     $ep = isset($_GET['ep']) ? $_GET['ep'] : null;
 
-    if ($ep) {
-
-        if (preg_match('/^e(\\d+)$/', $ep, $matches)) {
-
-            $meta = json_decode(file_get_contents("http://$localhost/api/episodes/$matches[1]"), true);
-            if ($meta) {
-                $content = str_replace("{{meta_title}}", htmlsafe($meta['name']), $content);
-                $content = str_replace("{{meta_description}}", htmlsafe($meta['intro']), $content);
-                $content = str_replace("{{meta_thumbnail}}", htmlsafe($meta['imageUrl']), $content);
-                $content = str_replace("{{meta_url}}", "http://$host/web/p$ch/$ep", $content);
-            }
-
-        } else if (preg_match('/^(\\d+)$/', $ep, $matches)) {
-
-            $meta = json_decode(file_get_contents("http://$localhost/api/programs/$matches[1]"), true);
-            if ($meta) {
-                $content = str_replace("{{meta_title}}", htmlsafe($meta['name']), $content);
-                $content = str_replace("{{meta_description}}", htmlsafe($meta['intro']), $content);
-                $content = str_replace("{{meta_thumbnail}}", htmlsafe($meta['imageUrl']), $content);
-                $content = str_replace("{{meta_url}}", "http://$host/web/p$ch/$ep", $content);
-            }
-
-        } else if (preg_match("/^yt(\\w+)$/", $ep, $matches)) {
-
-            $meta = json_decode(file_get_contents("http://gdata.youtube.com/feeds/api/videos/$matches[1]?alt=jsonc&v=2"), true);
-            if ($meta && $meta['data']) {
-                $data = $meta['data'];
-                $content = str_replace("{{meta_title}}", htmlsafe($data['title']), $content);
-                $content = str_replace("{{meta_description}}", htmlsafe($data['description']), $content);
-                $content = str_replace("{{meta_thumbnail}}", htmlsafe($data['thumbnail']['hqDefault']), $content);
-                $content = str_replace("{{meta_url}}", "http://$host/web/p$ch/$ep", $content);
-                $content = str_replace("{{meta_type}}", "video", $content);
-                $content = str_replace("{{meta_video}}", htmlsafe("http://www.youtube.com/v/$matches[1]?version=3&autohide=1"), $content);
-                $content = str_replace("{{meta_video_type}}", "application/x-shockwave-flash", $content);
-                $content = str_replace("{{meta_video_width}}", "480", $content);
-                $content = str_replace("{{meta_video_height}}", "360", $content);
-            }
-        }
-    }
-
     if ($ch && preg_match('/^\\d+$/', $ch)) {
 
-        $meta = json_decode(file_get_contents("http://$localhost/api/channels/$ch"), true);
-        if ($meta) {
-            $content = str_replace("{{meta_title}}", htmlsafe($meta['name']), $content);
-            $content = str_replace("{{meta_description}}", htmlsafe($meta['intro']), $content);
-            $content = str_replace("{{meta_thumbnail}}", htmlsafe($meta['imageUrl']), $content);
-            $content = str_replace("{{meta_url}}", "http://$host/web/$ch", $content);
+        if ($ep) {
+    
+            if (preg_match('/^e(\\d+)$/', $ep, $matches)) {
+    
+                $meta = json_decode(file_get_contents("http://$localhost/api/episodes/$matches[1]"), true);
+                if ($meta) {
+                    $content = str_replace("{{meta_title}}", htmlsafe($meta['name']), $content);
+                    $content = str_replace("{{meta_description}}", htmlsafe($meta['intro']), $content);
+                    $content = str_replace("{{meta_thumbnail}}", htmlsafe($meta['imageUrl']), $content);
+                    $content = str_replace("{{meta_url}}", "http://$host/web/p$ch/$ep", $content);
+                }
+    
+            } else if (preg_match('/^(\\d+)$/', $ep, $matches)) {
+    
+                $meta = json_decode(file_get_contents("http://$localhost/api/programs/$matches[1]"), true);
+                if ($meta) {
+                    $content = str_replace("{{meta_title}}", htmlsafe($meta['name']), $content);
+                    $content = str_replace("{{meta_description}}", htmlsafe($meta['intro']), $content);
+                    $content = str_replace("{{meta_thumbnail}}", htmlsafe($meta['imageUrl']), $content);
+                    $content = str_replace("{{meta_url}}", "http://$host/web/p$ch/$ep", $content);
+                }
+    
+            } else if (preg_match("/^yt(\\w+)$/", $ep, $matches)) {
+    
+                $meta = json_decode(file_get_contents("$ytGdata/feeds/api/videos/$matches[1]?$ytParam"), true);
+                if ($meta && $meta['data']) {
+                    $data = $meta['data'];
+                    $content = str_replace("{{meta_title}}", htmlsafe($data['title']), $content);
+                    $content = str_replace("{{meta_description}}", htmlsafe($data['description']), $content);
+                    $content = str_replace("{{meta_thumbnail}}", htmlsafe($data['thumbnail']['hqDefault']), $content);
+                    $content = str_replace("{{meta_url}}", "http://$host/web/p$ch/$ep", $content);
+                    $content = str_replace("{{meta_type}}", "video", $content);
+                    $content = str_replace("{{meta_video}}", htmlsafe("http://www.youtube.com/v/$matches[1]?version=3&autohide=1"), $content);
+                    $content = str_replace("{{meta_video_type}}", "application/x-shockwave-flash", $content);
+                    $content = str_replace("{{meta_video_width}}", "480", $content);
+                    $content = str_replace("{{meta_video_height}}", "360", $content);
+                }
+            }
+
+        } else {
+
+            $meta = json_decode(file_get_contents("http://$localhost/api/channels/$ch"), true);
+            if ($meta) {
+
+                if (preg_match($ytChannelRegex, $meta['sourceUrl'], $matches)) {
+
+                    $data = json_decode(file_get_contents("$ytGdata/feeds/api/partners/$matches[1]/branding/default?$ytParam"), true);
+                    if ($data && $data['entry'] && $data['entry']['yt$option'] && is_array($data['entry']['yt$option'])) {
+                        $ytOption = $data['entry']['yt$option'];
+                        for (int $i = 0; $i < count($ytOption); $i++) {
+                            if ($ytOption[$i] == 'channel.banner.tv.low.image.url')
+                                $content = str_replace('{{meta_thumbnail}}', $ytOption[$i]['$t'], $content);
+                        }
+                    } else {
+                        $data = json_decode(file_get_contents("$ytGdata/feeds/api/users/$matches[1]?$ytParam"), true);
+                        $content = str_replace('{{meta_thumbnail}}', $data['media$thumbnail']['url'], $content);
+                    }
+
+                } else if (preg_match($ytPlaylistRegex, $meta['sourceUrl'], $matches)) {
+
+                    $data = json_decode(file_get_contents("$ytGdata/feeds/api/playlists/$matches[1]?$ytParam"), true);
+                    if ($data && $data['feed'] && $data['feed']['media$group']['media$thumbnail'] &&
+                        is_array($data['feed']['media$group']['media$thumbnail'])) {
+                        
+                        $thumbnails = $data['feed']['media$group']['media$thumbnail'];
+                        usort($thumbnails, 'compare_thumbnail');
+                        $content = str_replace('{{meta_thumbnail}}', array_pop($thumbnails)['url'], $content);
+                    }
+
+                }
+
+                $content = str_replace("{{meta_title}}", htmlsafe($meta['name']), $content);
+                $content = str_replace("{{meta_description}}", htmlsafe($meta['intro']), $content);
+                $content = str_replace("{{meta_thumbnail}}", htmlsafe($meta['imageUrl']), $content);
+                $content = str_replace("{{meta_url}}", "http://$host/web/$ch", $content);
+            }
         }
     }
 
     $mso = json_decode(file_get_contents("http://$localhost/api/mso/$msoName"), true);
     if ($mso == null) {
-	    $mso = json_decode(file_get_contents("http://$localhost/api/mso/9x9"), true);
+        $mso = json_decode(file_get_contents("http://$localhost/api/mso/9x9"), true);
     }
     if ($mso['title']) $content = str_replace("{{meta_title}}", htmlsafe($mso['title']), $content);
     if ($mso['intro']) $content = str_replace("{{meta_description}}", htmlsafe($mso['intro']), $content);
