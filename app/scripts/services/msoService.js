c71edf7a12c2ac2000bfeae78063303b0629e740
Joe Chen <joe.9x9@gmail.com>
2014-02-13 11:15:48 +0800

    switching back to json for now

--- app/scripts/services/msoService.js
+++ app/scripts/services/msoService.js
@@ -4,7 +4,23 @@ ld.service("msoService", function($http){
 	this.get = function(){
 
 		var _d = $.Deferred();
-
+		var parseBrandInfo = function(csv){
+			var blocks = csv.split("--\n");
+			if(blocks[0].split("\t")[0] === "0"){
+				var data = blocks[1];
+				var arr = data.split("\n");
+				var obj = {}, tmp;
+				for(var i = 0; i<arr.length; i++){
+					tmp = arr[i].split("\t");
+					if(tmp.length === 2){
+						obj[tmp[0]] = tmp[1];
+					}
+				}
+				return obj;
+			}
+			console.error(csv);
+			return false;
+		}
 		if(loaded){
 			_d.resolve(data);
 		}else{
@@ -13,6 +29,7 @@ ld.service("msoService", function($http){
 			  	"url" : "scripts/data/cts.json"
 			})
 			.success(function(data){
+				//var brandInfo = parseBrandInfo(data);
 				self.data = data;
 				_d.resolve(data);
 				loaded = true;
