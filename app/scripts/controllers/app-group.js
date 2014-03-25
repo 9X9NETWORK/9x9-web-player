d0e7b0760de94c9a33856d8baf80190cfcbf533c
Joe Chen <joe.9x9@gmail.com>
2014-03-25 11:45:42 +0800

    try fix bug

--- app/scripts/controllers/app-group.js
+++ app/scripts/controllers/app-group.js
@@ -3,24 +3,29 @@
 ld.controller('AppGroupCtrl', function ($scope, msoService){
   	
 	msoService.get().then(function(data){
-		$scope.items = data.link;
+		
+		$scope.$apply(function(){
+			$scope.items = data.link;
+		});
+
 		for(var i = 0; i<data.link.length; i++){
+
 			if(data.link[i].ios == ""){
 				(function(num){
 					setTimeout(function(){
 						$("li.app-group-item").eq(num).find("a.app-item-ios").removeAttr("href");
-					},10);
+					},100);
 				})(i);
 			}
+
 			if(data.link[i].android == ""){
 				(function(num){
 					setTimeout(function(){
 						$("li.app-group-item").eq(num).find("a.app-item-android").removeAttr("href");
-					},10);
+					},100);
 				})(i);
 			}
 		}
-		//$scope.$apply();
 		// console.log($scope.items);
 	});
 	var getOs = function(){
